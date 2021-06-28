2acd943b0424eb24cef338af2d81aa3050950e06
Shana Matthews <shana.l.matthews@gmail.com>
2021-06-28 13:14:20 -0700

    add some docker alises and set -o vi

--- aliases.zsh
+++ aliases.zsh
@@ -2,3 +2,6 @@ alias myip="curl http://ipecho.net/plain; echo"
 alias aliases="vim ~/src/git/dotfiles/aliases.zsh"
 alias c=clear
 alias top=htop
+alias boost="docker start boost; docker attach boost"
+alias ubuntu="docker run -it --rm ubuntu"
+alias rwxws="docker run -it --rm rwxrob/workspace"
--- custom.zsh
+++ custom.zsh
@@ -1,1 +1,2 @@
 fortune | ponysay
+set -o vi
