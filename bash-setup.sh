e6721e86ba091b0668a4ad564438ce560f1665a2
Shana Matthews <shana.l.matthews@gmail.com>
2022-08-05 20:21:02 -0700

    bash setup script

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
+# you must clone your own dotfiles & set up ssh key before this works,
+# sorry
+
+DOTFILES_DIR="$HOME/src/git/dotfiles"
+
 # install basic utilities
-sudo apt install vim
-sudo apt install gnome-tweaks
-sudo apt install git
-sudo apt install curl
+sudo apt -y update
+sudo apt -y install vim
+sudo apt -y install gnome-tweaks
+sudo apt -y install git
+sudo apt -y install curl
+sudo apt -y install tmux
+
+# clone dotfiles
+# TBD
 
 # make sure the below env vars are set
 source ~/.bashrc
 
-# set up directories if they don't exist
-mkdir -p "$SCRIPTS"
-mkdir -p "$SNIPPETS"
+# symlink dotfiles
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# don't fuck up your bashrc backup if you're running this again
+BASHRCBACKUP="$HOME/.bashrcbackup"
+
+if [ ! -f "$BASHRCBACKUP" ]; then
+    mv ~/.bashrc ~/.bashrcbackup
+fi
+
+ln -s "$DOTFILES_DIR"/.bashrc ~/.bashrc
+ln -s "$DOTFILES_DIR"/.vimrc ~/.vimrc
+ln -s "$DOTFILES_DIR"/.tmux.conf ~/.tmux.conf
+
+# set up directories if they don't exist
+ln -s "$DOTFILES_DIR"/scripts "$SCRIPTS"
+ln -s "$DOTFILES_DIR"/snippets "$SNIPPETS"
+
+# install thefuck
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
