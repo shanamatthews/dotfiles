d0ac2b4fa00b0f9cd76c5961e9be611eab283ece
Shana Matthews <shana.l.matthews@gmail.com>
2022-08-07 18:46:36 -0700

    add creating vim folders to setup

--- bash-setup.sh
+++ bash-setup.sh
@@ -13,6 +13,10 @@ sudo apt -y install git
 sudo apt -y install curl
 sudo apt -y install tmux
 
+# install glow bc I'm extra
+echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
+
 # clone dotfiles
 # TBD
 
@@ -33,9 +37,16 @@ ln -s "$DOTFILES_DIR"/.vimrc ~/.vimrc
 ln -s "$DOTFILES_DIR"/.tmux.conf ~/.tmux.conf
 
 # set up directories if they don't exist
+# if you run this a second time you'll get weird nested scripts &
+# snippets dirs...
 ln -s "$DOTFILES_DIR"/scripts "$SCRIPTS"
 ln -s "$DOTFILES_DIR"/snippets "$SNIPPETS"
+
+mkdir -p "$HOME/.vim/.undo/"
+mkdir -p "$HOME/.vim/.backup/"
+mkdir -p "$HOME/.vim/.swp/"
 
 # install thefuck
 sudo apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck --user
+
